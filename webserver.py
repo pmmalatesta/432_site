9a8a65d59cfcf4962754d55b912b2dae1fd77bc0
minimally228 <pmmalatesta228@gmail.com>
2021-03-10 11:32:19 -0500

    changed port

--- webserver.py
+++ webserver.py
@@ -64,7 +64,7 @@ def main():
                     web.get('/like', like)])
 
     #web.run_app(app, host="0.0.0.0", port=80)
-    web.run_app(app,host="0.0.0.0", port=3000)
+    web.run_app(app,host="0.0.0.0", port=80)
     #print("Webserver 1.0")
 
 if __name__=="__main__":
