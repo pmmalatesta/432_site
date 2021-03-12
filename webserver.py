66587d69c4e512b0f4632c6063c8dbbdb5f1bd7e
minimally228 <pmmalatesta228@gmail.com>
2021-03-12 11:14:40 -0500

    changed host binding

--- webserver.py
+++ webserver.py
@@ -73,7 +73,7 @@ def main():
                     web.get('/like', like)])
 
     #web.run_app(app, host="0.0.0.0", port=80)
-    web.run_app(app,host="0.0.0.0", port=4000)
+    web.run_app(app,host="127.0.0.1", port=4000)
     #print("Webserver 1.0")
 
 if __name__=="__main__":
